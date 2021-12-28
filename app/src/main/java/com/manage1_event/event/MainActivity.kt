ea247f7c3982fbc42b1d3b2bd8423ed86d5bcbe1
AD <amanshdeep2@gmail.com>
2021-12-28 22:02:14 +0530

    Initial commit

--- app/src/main/java/com/manage1_event/event/MainActivity.kt
+++ app/src/main/java/com/manage1_event/event/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.manage1_event.event
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.manage1_event.event.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding:ActivityMainBinding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+         //to be done ,login activity
+
+        binding.register.setOnClickListener {
+            val intent= Intent(this,NavigationHostActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
