6a94e335e9d5b5b50a65902ff34265d5756feeb9
AD <amanshdeep2@gmail.com>
2021-12-31 20:21:45 +0530

    Initial commit

--- app/src/main/java/com/manage1_event/event/MainActivity.kt
+++ app/src/main/java/com/manage1_event/event/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.manage1_event.event
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.manage1_event.event.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    var _binding : ActivityMainBinding? = null
-    val binding get() = _binding!!
+    private var _binding : ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            val intent= Intent(this,NavigationHostActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
