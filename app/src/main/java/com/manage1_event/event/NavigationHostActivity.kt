353644f85ff1c4baf249633644dd453967f5168c
AD <amanshdeep2@gmail.com>
2021-12-31 12:29:26 +0530

    Initial commit

--- app/src/main/java/com/manage1_event/event/NavigationHostActivity.kt
+++ app/src/main/java/com/manage1_event/event/NavigationHostActivity.kt
@@ -1,29 +1,38 @@
 package com.manage1_event.event
 
-import android.content.Intent
-import android.net.Uri
+import com.google.android.material.navigation.NavigationView
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
+import android.content.Intent
+
 import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+
 import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import com.google.android.material.navigation.NavigationView
+
 
 import com.manage1_event.event.databinding.ActivityNavigationHostBinding
-import java.lang.reflect.Array
+
+//this activity acts just as a fragment holder
+
+class NavigationHostActivity : AppCompatActivity() {
+    lateinit var navController: NavController
+    lateinit var drawerLayout: DrawerLayout
+
 
 //this activity acts just as a fragment holder
 
 class NavigationHostActivity : AppCompatActivity()  {
     lateinit var navController: NavController
     lateinit var drawerLayout: DrawerLayout
-    lateinit var navigationView:NavigationView
+    lateinit var navigationView: NavigationView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,13 +40,22 @@ class NavigationHostActivity : AppCompatActivity()  {
 
        buildDrawerLayout(binding)
     }
-    private fun buildDrawerLayout(binding: ActivityNavigationHostBinding){
-        navController=this.findNavController(R.id.host_fragment)
+
+
+
+  fun buildDrawerLayout(binding: ActivityNavigationHostBinding){
+       navController=this.findNavController(R.id.host_fragment)
         drawerLayout=binding.drawerLayout
         NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
         NavigationUI.setupWithNavController(binding.navigationView,navController)
 
     }
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,drawerLayout)
+    }
+}
 
 
 
@@ -59,6 +77,8 @@ class NavigationHostActivity : AppCompatActivity()  {
     fun logout(item:MenuItem){
 Toast.makeText(application.applicationContext,"Implement logout",Toast.LENGTH_SHORT).show()
     }
+
+
     fun contact(item: MenuItem){
 
         val intent = Intent(Intent.ACTION_SEND).apply {
@@ -73,6 +93,7 @@ Toast.makeText(application.applicationContext,"Implement logout",Toast.LENGTH_SH
 
         }
 }
+
 
 
 
